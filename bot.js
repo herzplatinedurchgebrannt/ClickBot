a126b802027e104d87f1a5463abbf6ed7f035e24
LX <alexandermathieu@googlemail.com>
2022-03-04 15:48:39 +0100

    read json and send

--- bot.js
+++ bot.js
@@ -1,27 +1,27 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
+const {fs} = require('file-system')
+
 let driver;
+//let dealAlarm = 'https://open.spotify.com/artist/2ZHNNNUkWlcboai09BqLRa';
 
-//<button data-testid="play-button" aria-label="Play" class="Button-qlcn5g-0 bqeOGM"><div class="ButtonInner-sc-14ud5tc-0 hgKCcx encore-bright-accent-set"><span aria-hidden="true" class="IconWrapper__Wrapper-sc-1hf1hjl-0 iWTrNM"><svg role="img" height="28" width="28" viewBox="0 0 24 24" class="Svg-sc-1bi12j5-0 hDgDGI"><path d="M7.05 3.606l13.49 7.788a.7.7 0 010 1.212L7.05 20.394A.7.7 0 016 19.788V4.212a.7.7 0 011.05-.606z"></path></svg></span></div></button>
+const accounts = JSON.parse(fs.readFileSync("./accounts.json", {encoding:'utf8', flag:'r'}));
+const playlist = JSON.parse(fs.readFileSync("./playlist.json", {encoding:'utf8', flag:'r'}));
 
-let dealAlarm = 'https://open.spotify.com/artist/2ZHNNNUkWlcboai09BqLRa';
-//onetrust-accept-btn-handler
-openGoogle();
+startSpotify(accounts.spotify[0],playlist.spotify[0]);
 
-async function openGoogle(){
+
+async function startSpotify(account, song){
+
+    console.log(account);
+    console.log(song);
+
+    const sessionUser = account.email;
+    const sessionPasswort = account.passwort;
+    const sessionSong = song.link;
+
     driver = await new Builder().forBrowser("chrome").build();
-    /*
-    await driver.get('http://www.google.com');
-    await click('Ich stimme zu');
-    await driver.findElement(By.name('q')).sendKeys('Developer Akademie', Key.RETURN);
-
-    await click('Developer Akademie - Karriere');
-    await click('Auswahl erlauben');*/
-
     
     await driver.get('https://www.spotify.com');
-    //await driver.findElement(By.id('onetrust-accept-btn-handler')).click();
-    //await click('Cookies akzeptieren');
-    //await driver.findElement(By.className('Qt5xfSWikz6CLU8Vobxs jzic9t5dn38QUOYlDka0')).click();
 
     await sleep(3000);
     await driver.findElement(By.id('onetrust-accept-btn-handler')).click();
@@ -30,13 +30,13 @@ async function openGoogle(){
     await click('Anmelden');
 
     await sleep(1000);
-    await driver.findElement(By.id('login-username')).sendKeys('herzplatine@web.de', Key.RETURN);
-    await driver.findElement(By.id('login-password')).sendKeys('herzplatine', Key.RETURN);
+    await driver.findElement(By.id('login-username')).sendKeys(sessionUser, Key.RETURN);
+    await driver.findElement(By.id('login-password')).sendKeys(sessionPasswort, Key.RETURN);
 
 
     await sleep(1000);
 
-    await driver.get(dealAlarm);
+    await driver.get(sessionSong);
 
     await sleep(3000);
     
